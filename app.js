aa42ded8d7ad822fd1381dd1dafd95e7476ed3c2
Maidest <michals1099@gmail.com>
2019-07-15 14:25:55 +0200

    Setup home route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','pug');
 
+//Home route
+app.get('/',(req, res) => {
+    res.render('index');
+})
+
 //Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
