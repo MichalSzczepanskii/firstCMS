a62e3fb4dd91020a2882af516e56c4fcb885c9ae
Maidest <michals1099@gmail.com>
2019-07-15 20:22:44 +0200

    Setup validator module

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//Setup validation
+const { check, validationResult } = require('express-validator');
+
 router.get('/register', (req, res) => {
     res.render('register');
 })
