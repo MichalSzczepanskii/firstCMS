f1b80ea32fdb918a14481bb59d5361bcdb688f5c
Maidest <michals1099@gmail.com>
2019-07-15 14:21:36 +0200

    Setup view engine

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','pug');
 
+//Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
